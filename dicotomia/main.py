58b5a94643217dd3448874a708402fc6559e805d
viniciuslimafernandes <viniciuslima.4k@gmail.com>
2020-03-10 20:07:09 -0300

    Otimizando codigo

--- dicotomia/main.py
+++ dicotomia/main.py
@@ -1,10 +1,7 @@
 import math
 
-# a = float(input("Digite o valor de a: "))
-# b = float(input("Digite o valor de b: "))
-# e = float(input("Digite o valor da precisão(e): "))
 def f(x):
-    return (pow(x,2) - 5) #função trabalhada na sala
+    return (pow(x,2) - 5) #função
 
 
 def verifica_intervalo(a,b):
@@ -20,25 +17,18 @@ def calcula_raiz(a, b, e):
         while (math.fabs(b-a)/2 >= e):
             i_meio = (a+b)/2
             if f(i_meio) == 0:
-                print("************************")
-                print("Valor da raiz: ", round(i_meio, 5))
-                print("************************")
+                print("Valor da raiz: ", round(i_meio, 5), "\n")
                 break
             elif f(a) * f(i_meio) < 0:
                 b = i_meio
             else: 
                 a = i_meio
-        print("************************")
-        print("Valor da raiz: ", round(i_meio, 5))
-        print("************************")
+        print("Valor da raiz: ", round(i_meio, 5), "\n")
         
-    else:
-        print("************************")       
-        print("Intervalo não existe")
-        print("************************")
+    else:       
+        print("Intervalo não existe", "\n")
         
 #valores
 calcula_raiz(1,6, 0.01)
 calcula_raiz(1,8, 0.01)
 calcula_raiz(2,3, 0.01)      
-calcula_raiz(4,6, 0.01) 
